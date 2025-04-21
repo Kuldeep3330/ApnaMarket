ea2734f172aa9fae72cef2406d8a1ede5e9be7bd
Kuldeep Kumar <upsainian3330@gmail.com>
2025-04-21 22:57:41 +0530

    updateOrderStatus controller

--- server/controllers/order.controller.js
+++ server/controllers/order.controller.js
@@ -76,3 +76,19 @@ export const getAllOrders = async (req, res) => {
       res.status(500).json({ error: 'Failed to fetch all orders' });
     }
   };
+
+//change order status 
+export const updateOrderStatus = async (req, res)=>{
+    try {
+        const order = await Order.findById(req.params.id);
+
+        if (!order) return res.status(404).json({ error: 'Order not found' });
+
+        order.status = req.body.status
+
+        await order.save();
+        res.status(200).json({ message: 'Order status updated', order });
+    } catch (err) {
+        res.status(500).json({ error: 'Failed to update order status' });
+    }
+}
--- server/routes/order.router.js
+++ server/routes/order.router.js
@@ -1,6 +1,6 @@
 import express from 'express'
 
-import { placeOrder, getUserOrders, getOrderById, getAllOrders } from '../controllers/order.controller.js'
+import { placeOrder, getUserOrders, getOrderById, getAllOrders, updateOrderStatus } from '../controllers/order.controller.js'
 
 const router = express.Router();
 
@@ -9,7 +9,7 @@ router.get('/', getUserOrders)
 router.get('/:id', getOrderById)
 
 router.get('/admin/orders', getAllOrders)
-
+router.put('/admin/orders/:id', updateOrderStatus)
 
 
 export default router
