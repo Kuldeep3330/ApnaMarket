a8d1588799af9edf3684e3dc328e73a46d994d47
Kuldeep Kumar <upsainian3330@gmail.com>
2025-04-21 14:07:26 +0530

    cart.model.js

---
+++ server/models/cart.model.js
@@ -0,0 +1,27 @@
+// models/CartItem.js
+import mongoose, {Schema} from 'mongoose';
+
+const cartItemSchema = new Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true,
+  },
+
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1
+  }
+
+}, {
+  timestamps: true
+});
+
+export const CartItem= mongoose.model('CartItem', cartItemSchema);
