d609672137fd3804d0d9e77d90590e50d832adeb
Kuldeep Kumar <upsainian3330@gmail.com>
2025-04-21 14:20:08 +0530

    order.model.js

---
+++ server/models/order.model.js
@@ -0,0 +1,42 @@
+import mongoose, {Schema} from "mongoose";
+
+const orderschema= new Schema({
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+      },
+      quantity: {
+        type: Number,
+        required: true,
+        min: 1
+      },
+      priceAtPurchase: {
+        type: Number,
+        required: true
+      }
+    });
+    
+    const orderSchema = new mongoose.Schema({
+      userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+      },
+    
+      items: [orderItemSchema],
+    
+      total: {
+        type: Number,
+        required: true
+      },
+    
+      status: {
+        type: String,
+        enum: ['pending', 'shipped', 'delivered'],
+        default: 'pending'
+      }
+
+},{timestamps:true})
+
+export const Order= mongoose.model("Order", orderSchema)
