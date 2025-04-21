c851fcc73671601c3334e298c342d31f97280f19
Kuldeep Kumar <upsainian3330@gmail.com>
2025-04-21 14:39:10 +0530

    jsonwebtoken and bcryptjs comes into picture

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken'
 
 const userSchema= new Schema({
     name:{
@@ -25,5 +27,25 @@ const userSchema= new Schema({
       }
 
 },{timestamps:true})
+
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")) return next()
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()    
+})
+
+// JWT token method
+userSchema.methods.generateJWT = function () {
+    return jwt.sign(
+      { _id: this._id, email: this.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+  };
+
+userSchema.methods.isPasswordCorrect= async function(password){
+    return await bcrypt.compare(password, this.password);
+}
 
 export const User= mongoose.model("User", userSchema)
